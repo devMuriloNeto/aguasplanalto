47974029c29ba9814ffd5f926377c6148cb8587a
Murilo <murilonunesneto@gmail.com>
2025-10-24 14:01:33 -0300

    incluindo botão futuante do whatsapp no script

--- whatsapp.js
+++ whatsapp.js
@@ -4,3 +4,7 @@ const whatsappLink = `https://wa.me/${whatsappNumber}`;
 document.querySelectorAll('.btn-whatsapp-nav').forEach(btn => {
   btn.setAttribute('href', whatsappLink);
 });
+
+document.querySelectorAll('.whatsapp-button-float').forEach(btn => {
+  btn.setAttribute('href', whatsappLink);
+});
