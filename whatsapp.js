1e6a7f5c19a10b195e049337e1f3708e1c4ca76e
Murilo Neto <124456441+devMuriloNeto@users.noreply.github.com>
2025-10-31 12:03:43 -0300

    atualização script wpp para usar ID invés de class

--- whatsapp.js
+++ whatsapp.js
@@ -1,10 +1,6 @@
 const whatsappNumber = "5511971126491"; // Altere aqui quando precisar
 const whatsappLink = `https://wa.me/${whatsappNumber}`;
 
-document.querySelectorAll('.btn-whatsapp-nav').forEach(btn => {
-  btn.setAttribute('href', whatsappLink);
-});
-
-document.querySelectorAll('.whatsapp-button-float').forEach(btn => {
+document.querySelectorAll('#wpp').forEach(btn => {
   btn.setAttribute('href', whatsappLink);
 });
